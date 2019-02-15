84a8cbc2a3ef1e42cb9f1b7ac3a0dcd2d2af59d4
nholtzerresearch <nholtzer@math.arizona.edu>
2019-02-15 20:28:33 +0000

    added a bunch of code changes

--- Research/gravwave-1.cc
+++ Research/gravwave-1.cc
@@ -7,6 +7,7 @@
 
 
 //Include files
+#include <deal.II/base/utilities.h>
 #include <deal.II/base/function.h>
 #include <deal.II/base/quadrature_lib.h>
 #include <deal.II/base/smartpointer.h>
@@ -22,7 +23,7 @@
 
 #include <deal.II/grid/grid_generator.h>
 #include <deal.II/grid/tria.h>
-
+#include <deal.II/grid/tria_boundary_lib.h>
 #include <deal.II/lac/constraint_matrix.h>
 #include <deal.II/lac/dynamic_sparsity_pattern.h>
 #include <deal.II/lac/solver_gmres.h>
@@ -32,6 +33,7 @@
 
 #include <deal.II/numerics/data_out.h>
 #include <deal.II/numerics/vector_tools.h>
+#include <deal.II/numerics/matrix_tools.h>
 
 //Standard C++ libraries
 #include <stdio.h>
@@ -84,13 +86,28 @@ public:
  dealii::SparsityPattern           sparsity_pattern_;
  dealii::ConstraintMatrix          affine_constraints_;
 
+ SparseMatrix<double>              mass_matrix_;
+ SparseMatrix<double>              laplace_matrix_;
+ SparseMatrix<double>		   system_matrix_;
+ SparseMatrix<double>              system_RHS_;
  std::vector<double>               nodeLocation;//ADDED FOR DIRICH BCS
  std::map<unsigned int,double>     boundary_values;//ADDED FOR DIRICH BCS
  
- double R, g1, g2;
-// double time_step_, time_;
-// unsigned int timestep_number_;
-// const double theta_;
+
+ dealii::Vector<value_type>        old_solution_;
+ dealii::Vector<double>		   solution_;
+ dealii::Vector<double>            system_right_hand_side_; 
+ 
+// dealii::SparseMatrix<double> system_matrix_;
+
+// dealii::Vector<double> system_right_hand_side_;
+// dealii:: Vector<double> solution_;
+
+
+ double R, bc_left, bc_right;
+ double time_step_, time_;
+ unsigned int timestep_number_;
+ const double theta_;
 
 //};
   // PARAMETER
@@ -118,7 +135,7 @@ public:
    return (2.0*f_func(point))/point[0] + 2.0 * imag*(q*Q)/point[0] +fprime_func(point);
    };
 
- std::function<value_type(const Point<dim> &point)>A_coefficient=
+ std::function<value_type(const Point<dim> &point)>a_coefficient=
    [&](const Point<dim> & point) {
    //
    double Q = 1.0;
@@ -127,19 +144,19 @@ public:
    return (2.0/point[0])*(f_func(point) +1.0) + (2.0 * imag*(q*Q))/point[0] ;
    };
 
-  std::function<value_type(const Point<dim> &point)>B_coefficient=
+  std::function<value_type(const Point<dim> &point)>b_coefficient=
     [&](const Point<dim> & point) {
     //
     return (2.0 + f_func(point)) + 0.0 * imag;
     };
 
- std::function<value_type(const Point<dim> &point)>C_coefficient=
+ std::function<value_type(const Point<dim> &point)>c_coefficient=
    [&](const Point<dim> & point) {
    //
    return (2.0/point[0]) + 0.0 * imag;
    };
 
-  std::function<value_type(const Point<dim> &point)>D_coefficient=
+  std::function<value_type(const Point<dim> &point)>d_coefficient=
     [&](const Point<dim> & point) {
     //
     double Q=1.0;
@@ -148,15 +165,100 @@ public:
     };
    
 
-    dealii::SparseMatrix<double> system_matrix_;
+    //dealii::SparseMatrix<double> system_matrix_;
 
-    dealii::Vector<double> system_right_hand_side_;
-    dealii:: Vector<double> solution_;
+   // dealii::Vector<double> system_right_hand_side_;
+   // dealii:: Vector<double> solution_;
 
    };
+//@sect{IC,RHS,BCs}
+//Here we declare three more classes for the implementation of the IC, RHS, and non-homogeneous Dirichlet BCs.
+template<int dim>
+class Initial : public Function<dim>
+{
+public:
+  Initial (const unsigned int n_components =1, 
+               const double time=0.0): Function<dim>(n_components, time){}
+  virtual double value (const Point<dim> &p,
+                        const unsigned int component =0) const;
+};
+
+template<int dim>
+double Initial<dim>::value (const Point<dim> &p,
+                            const unsigned int ) const
+{
+  double t = this->get_time ();
+
+       double a = 1.0;
+       double mu = 1.0;
+       double sigma = 1.0;
+      
+       return a.*std::exp(-((p[0]-mu)-t)*((p[0]-mu)-t)/(2.*sigma*sigma));
+}
+
+template<int dim>
+class InitialValues : public Function<dim>
+{
+public:
+  InitialValues (const unsigned int n_components =1,
+	         const double time =0.0)
+    :
+    Function<dim>(n_components, time)
+   {}
+
+   virtual double value (const Point<dim> &p,
+                         const unsigned int component) const;
+};
+
+template <int dim>
+double InitialValues<dim>::value (const Point<dim> &p,
+             			  const unsigned int component) const
+{
+  return Initial<dim>(1, this->get_time()).value (p, component);
+} 
 
 
+template <int dim>
+class RightHandSide : public Function<dim>
+{
+public:
+ RightHandSide () : Function<dim>() {}
 
+ virtual double value (const Point<dim> &p,
+                       const unsigned int component=0) const;
+};
+
+/*template <int dim>
+class BoundaryValues : public Function <dim>
+{
+public:
+ BoundaryValues () : Function<dim>() {}
+
+ virtual double value (const Point<dim> &p,
+                       const unsigned int component=0) const;
+};*/
+
+template <int dim>
+double RightHandSide<dim>::value (const Point<dim> &p,
+                                  const unsigned int) const
+{
+  double return_value = 0.0;
+
+  return return_value;
+}
+
+
+/*template<int dim>
+double BoundaryValues<dim>::value (const Point<dim> &p,
+                                   const unsigned int) const
+{
+  if (p[0]=0.001){
+    return bc_left; //PARAMETER TO CHANGE POSSIBLY
+  }
+  if(p[0]=1){
+    return bc_right;//PARAMETER
+  }
+}*/
 //@sect{GravWave::GravWave}
 
 template <int dim>
@@ -165,11 +267,11 @@ GravWave<dim>::GravWave()
    : 
    finite_element_(FE_Q<dim>(1), 2),
    mapping_(1),    // PARAMETER
-   quadrature_(3) // PARAMETER
-  // time_step_(1./64), //PARAMETER
-  // time_(time_step_),
-  // timestep_number_(1),
-  // theta_(0.5)
+   quadrature_(3), // PARAMETER
+   time_step_(1./64), //PARAMETER
+   time_(time_step_),
+   timestep_number_(1),
+   theta_(0.5)
 {} 
 
 
@@ -199,10 +301,10 @@ const unsigned int totalNodes = dof_handler_.n_dofs(); //Total number of nodes
 
  for(unsigned int globalNode=0; globalNode<totalNodes; globalNode++){
     if(nodeLocation[globalNode] == 0){
-      boundary_values[globalNode] = g1;
+      boundary_values[globalNode] = bc_left;;
     }
     if(nodeLocation[globalNode] == R){ 
-      boundary_values[globalNode] = g2;
+      boundary_values[globalNode] = bc_right;
       }
     }
 }
@@ -216,9 +318,7 @@ void
 GravWave<dim>::setup_system()
 {
   //Define constants for problem (Dirichlet boundary values) ADDED FOR DIRICH BC
-  g1 =0.001 ; g2 =1 ;
- std::cout << "g1= "<< g1;
- std::cout << "g2= "<< g2;
+ bc_left =0.001 ; bc_right =1 ;
  std::cout << "GravWave<dim>::setup_system()" << std::endl;
 
  dof_handler_.initialize(triangulation_, finite_element_);
@@ -239,9 +339,17 @@ GravWave<dim>::setup_system()
                                  false);
  sparsity_pattern_.copy_from(c_sparsity);
  
+// mass_matrix_.reinit(sparsity_pattern_);
+// laplace_matrix_.reinit(sparsity_pattern_);
  system_matrix_.reinit(sparsity_pattern_);
+ system_RHS_.reinit(sparsity_pattern_);
+// MatrixCreator::create_mass_matrix(dof_handler_, quadrature_, mass_matrix_);
+
+// MatrixCreator::create_laplace_matrix(dof_handler_, quadrature_, laplace_matrix_);
+
  system_right_hand_side_.reinit(dof_handler_.n_dofs());
  solution_.reinit(dof_handler_.n_dofs());
+ old_solution_.reinit(dof_handler_.n_dofs());
 }
 // @sect{GravWave::setup_constraints}
 
@@ -253,10 +361,10 @@ GravWave<dim>::setup_constraints()
   std::cout << "GravWave<dim>::setup_constraints()" << std::endl;
   affine_constraints_.clear();
 
-  VectorTools::interpolate_boundary_values(dof_handler_,
+ /* VectorTools::interpolate_boundary_values(dof_handler_,
                                            0,
                                            ZeroFunction<dim>(2),
-                                           affine_constraints_);
+                                           affine_constraints_);*/
 
   DoFTools::make_hanging_node_constraints(dof_handler_, affine_constraints_);
 
@@ -273,12 +381,21 @@ GravWave<dim>::assemble_system()
   deallog << "GravWave<dim>::assemble_system()" << std::endl;
 
   this->system_matrix_          = 0.;
+  this->system_RHS_             = 0.;
   this->system_right_hand_side_ = 0.;
   this->solution_               = 0.;
 
   const unsigned int dofs_per_cell = finite_element_.dofs_per_cell;
 
   FullMatrix<double> cell_matrix(dofs_per_cell, dofs_per_cell);
+  FullMatrix<double> M_matrix_one(dofs_per_cell, dofs_per_cell);
+  FullMatrix<double> M_matrix_two(dofs_per_cell, dofs_per_cell);
+  FullMatrix<double> K_matrix_one(dofs_per_cell, dofs_per_cell);
+  FullMatrix<double> K_matrix_two(dofs_per_cell, dofs_per_cell);
+  FullMatrix<double> A_matrix(dofs_per_cell, dofs_per_cell);
+  FullMatrix<double> cell_rhs_matrix(dofs_per_cell, dofs_per_cell);
+
+
   Vector<double>     cell_rhs(dofs_per_cell);
 
   FEValues<dim> fe_values(mapping_,
@@ -297,6 +414,12 @@ GravWave<dim>::assemble_system()
   for (auto cell : dof_handler_.active_cell_iterators())
     {
       cell_matrix = 0.;
+      cell_rhs_matrix = 0;
+      M_matrix_one = 0.;
+      M_matrix_two = 0.;
+      K_matrix_one = 0.; 
+      K_matrix_two = 0.;
+      A_matrix = 0.;
       cell_rhs    = 0.;
       
       fe_values.reinit(cell);
@@ -310,7 +433,10 @@ GravWave<dim>::assemble_system()
       for (unsigned int q_point = 0; q_point < n_q_points; ++q_point)
         {
           const auto position = quadrature_points[q_point];
-          const auto A_coef = A_coefficient(position);
+          const auto a_coef = a_coefficient(position);
+          const auto b_coef = b_coefficient(position);
+          const auto c_coef = c_coefficient(position);
+          const auto d_coef = d_coefficient(position);
 
           const auto JxW = fe_values.JxW(q_point);
 
@@ -326,12 +452,19 @@ GravWave<dim>::assemble_system()
               cell_rhs(i) += (value_i * JxW).real();
 
               for (unsigned int j = 0; j < dofs_per_cell; ++j)
-                {
+                { 
+                  const auto value_j = rot_x * real_part.value(j, q_point) +
+                                   rot_y * imag_part.value(j, q_point);
+
                   const auto grad_j = real_part.gradient(j, q_point) +
                                       imag * imag_part.gradient(j, q_point);
-
-                  cell_matrix(i, j) += (grad_j * A_coef * grad_i).real();
-
+                  M_matrix_one(i,j) += (value_j * (c_coef+time_step_*theta_) * value_i).real();
+                  M_matrix_two(i,j) += (value_j * (c_coef- time_step_* (1.-theta_)*d_coef) * value_i).real();
+                  K_matrix_one(i,j) += (value_j *  2.* grad_i).real(); 
+                  K_matrix_two(i,j) += (value_j * (2. + time_step_ * theta_ * a_coef) * grad_i).real();
+                  A_matrix(i,j)     += (grad_j * (time_step_ * theta_ * b_coef) * grad_i).real();
+                  cell_matrix(i, j) += (M_matrix_one(i,j) + K_matrix_one(i,j));
+                  cell_rhs_matrix(i,j) += (M_matrix_two(i,j) + K_matrix_two(i,j) - A_matrix(i,j));
                 } // for j 
 
             }     // for i 
@@ -343,12 +476,31 @@ GravWave<dim>::assemble_system()
                                                      local_dof_indices,
                                                      system_matrix_,
                                                      system_right_hand_side_);
+       /* affine_constraints_.distribute_local_to_global(cell_matrix,
+                                                     cell_rhs_matrix,
+                                                     local_dof_indices,
+                                                     system_matrix_,
+                                                     system_RHS_);*/
+
     } // loop over dof_handler iterators 
    // MatrixTools::apply_boundary_values (boundary_values, cell_matrix, false);ADDED FOR BCS
-  }
+      MatrixTools::apply_boundary_values (boundary_values,
+                                    system_matrix_,//ADDED FOR BCS
+                                    solution_,
+                                    system_right_hand_side_);
 
+//  }
 
-
+//std::map<types::global_dof_index,double> boundary_values;
+/*VectorTools::interpolate_boundary_values (dof_handler_,
+					  0,
+					  BoundaryValues<dim>(),
+					  boundary_values);*/
+/*MatrixTools::apply_boundary_values (boundary_values, 
+				    system_matrix_,//ADDED FOR BCS
+				    solution_,
+				    system_right_hand_side_);*/
+}
 
 
 
@@ -366,7 +518,7 @@ GravWave<dim>::solve()
   SparseDirectUMFPACK solver;
   solver.initialize(system_matrix_);
   solver.vmult(solution_, system_right_hand_side_);
-
+  //solver.vmult(solution_, system_RHS_);
   affine_constraints_.distribute(solution_);
 }
 
@@ -399,7 +551,7 @@ GravWave<dim>::output_result()
 int main()
 
 {
-  static constexpr int dim = 2;
+ // static constexpr int dim = 2;
  // GravWave<dim> gravwave_problem;
 
  // gravwave_problem.run();
