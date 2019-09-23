cfed8a1caeb448d41ef5705dd2df8d3d2c802d2b
Matthias Maier <tamiko@43-1.org>
2019-09-23 17:10:10 -0500

    add initial and boundary values

--- code-v2/gravwave.cc
+++ code-v2/gravwave.cc
@@ -19,6 +19,18 @@ public:
   Coefficients()
     : ParameterAcceptor("A - Coefficients")
   {
+    R_0 = 0.001;
+    add_parameter("R_0", R_0, "inner radius of the computational domain");
+
+    R_1 = 0.0186;
+    add_parameter("R_1", R_1, "outer radius of the computational domain");
+
+    Psi_0 = 0.001;
+    add_parameter("Psi_0", Psi_0, "inner boundary value");
+
+    Psi_1 = 0.001;
+    add_parameter("Psi_1", Psi_1, "outer boundary value");
+
     M = 0.2;
     add_parameter("M", M, "mass of the black hole");
 
@@ -27,6 +39,9 @@ public:
 
     q = 1.;
     add_parameter("q", q, "charge density");
+
+    Mu = 2.;
+    add_parameter("Mu", Mu, "mu parameter of the initial envelope");
 
 
     f = [&](double r) {
@@ -48,12 +63,32 @@ public:
     c = [&](double r) { return 2. / r; };
 
     d = [&](double r) { return imag * q * Q / (r * r); };
+
+    initial_values = [&](double r) {
+      Assert(r >= R_0 && r <= R_1, ExcMessage("Radius r is out of range"));
+      return Psi_0 * (r - R_1) / (R_0 - R_1) + Psi_1 * (r - R_0) / (R_1 - R_0) +
+             Mu * std::cos(M_PI * (R_1 - R_0) / 2. * (r - (R_1 + R_0) / 2.));
+    };
   }
+
+  /* Publicly readable parameters: */
+
+  double R_0;
+  double R_1;
+
+  double Psi_0;
+  double Psi_1;
+
+  double Mu;
+
+  /* Publicly readable functions: */
 
   std::function<value_type(double)> a;
   std::function<value_type(double)> b;
   std::function<value_type(double)> c;
   std::function<value_type(double)> d;
+
+  std::function<value_type(double)> initial_values;
 
 private:
 
